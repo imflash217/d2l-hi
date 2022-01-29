45efda1f3bcd619b7805103c147f19114a9102b6
imflash217 <12234745+imflash217@users.noreply.github.com>
2022-01-29 18:56:46 -0500

    update

--- src/utils.py
+++ src/utils.py
@@ -7,4 +7,14 @@ import requests
 DATA_HUB = dict()
 DATA_URL = f"http://d2l-data.s3-accelerate.amazonaws.com/"
 
+def download(name:str, cache_dir:str=os.path.join("../../../", "data")):
+    """
+    Downloads the file inserte dinto DATA_HUB.
+    Returns:
+        the local filename
+    """
+    assert name in DATA_HUB, f"{name} does not exist in {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.maakedirs(cache_dir, exists_ok=True)
+    fname = os.path.join(cache_dir, url.split("/")[-1])
 
